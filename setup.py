f954247b7e47ea0642b86b1bee36b88363c70602
Andy Kipp <anki-code@users.noreply.github.com>
2024-05-03 00:13:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except (IOError, OSError):
 
 setup(
     name='xontrib-pipeliner',
-    version='0.4.3',
+    version='0.4.4',
     license='BSD',
     author='anki',
     author_email='author@example.com',
