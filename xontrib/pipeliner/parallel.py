dc0095085dda1592ba83caf94b86222ef9704b62
Andy Kipp <anki-code@users.noreply.github.com>
2024-05-03 00:13:09 +0200

    Update parallel.py

--- xontrib/pipeliner/parallel.py
+++ xontrib/pipeliner/parallel.py
@@ -19,7 +19,7 @@ class PipelinerParallel(object):
        try:
            return eval(self.code, ctx)
        except:
-           print_color('{YELLOW}' + f'Error line {args[1]+1}: {args[0]}', file=sys.stderr)
+           print_color('{YELLOW}' + f'Error line {args[1]+1}: {args[0]!r}', file=sys.stderr)
            print_color('{YELLOW}' + str(traceback.format_exc()), file=sys.stderr)           
            return None
 
